303bb00cc7e450254b7366e815a2bd67bb68e79a
coderrakibul <coder.rakibul@gmail.com>
2022-04-04 01:16:04 +0600

    nav bar design completed

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import Blogs from './component/Blogs/Blogs';
 import Dashboard from './component/Dashboard/Dashboard';
 import Header from './component/Header/Header';
 import Home from './component/Home/Home';
+import NotFound from './component/NotFound/NotFound';
 import Review from './component/Review/Review';
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div>
@@ -14,9 +16,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="home" element={<Home />} />
         <Route path="/review" element={<Review />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />}/>
         <Route path="/blogs" element={<Blogs/>} />
         <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound/>}></Route>
       </Routes>
     </div>
   );
--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
+        <div className='nav-container'>
             <Link to="/">HOME</Link>
             <Link to="/review">REVIEWS</Link>
             <Link to="/dashboard">DASHBOARD</Link>
