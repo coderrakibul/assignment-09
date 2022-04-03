90fc6a7f93b8b5fb602ff847c95c65c051416f79
coderrakibul <coder.rakibul@gmail.com>
2022-04-03 23:39:00 +0600

    router installed and setup completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import About from './component/About/About';
+import Blogs from './component/Blogs/Blogs';
+import Dashboard from './component/Dashboard/Dashboard';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
+import Review from './component/Review/Review';
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="home" element={<Home />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/blogs" element={<Blogs/>} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/component/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Link to="/">HOME</Link>
+            <Link to="/review">REVIEWS</Link>
+            <Link to="/dashboard">DASHBOARD</Link>
+            <Link to="/blogs">BLOGS</Link>
+            <Link to="/about">ABOUT</Link>
+        </div>
+    );
+};
+
+export default Header;
