0e80cb54a5771a75fbb533627df9721b2f932725
coderrakibul <coder.rakibul@gmail.com>
2022-04-04 02:17:20 +0600

    home page top part is ready

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import image from './image/dslr.jpg';
+import './Home.css';
+
 
 const Home = () => {
     return (
-        <div>
-            <h2> this is home page</h2>
+        <div className='container d-flex mt-5 align-items-center justify-content-between'>
+            <div className="camera-text">
+                <h1 className='camera-title-one'>Do Something</h1>
+                <h1 className='camera-title-two'>With Your gorgeous Camera</h1>
+                <p className='fs-5'>Hey! Introducing our new awesome product. Buy this and enjoy your time. This product really gives you good feelings. You can handle this very easily. Don't be late!</p>
+
+            </div>
+            <div className="camera-image">
+                <img className='w-100' src={image} alt="" />
+            </div>
         </div>
     );
 };
