11cb6910d977e30d81c689db8b6c85568ebc11bf
coderrakibul <coder.rakibul@gmail.com>
2022-04-04 14:04:58 +0600

    bar chart created

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import './Dashboard.css'
 
 const Dashboard = () => {
@@ -44,15 +44,30 @@ const Dashboard = () => {
     ];
 
     return (
-        <LineChart className='container line-chart mt-5' width={730} height={250} data={data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey={"month"} />
-            <YAxis dataKey={"investment"} />
-            <Tooltip dataKey={"revenue"} />
-            <Line type="monotone" dataKey={"sell"} stroke="#8884d8" />
-            <Line type="monotone" dataKey={"revenue" }stroke="#82ca9d" />
-        </LineChart>
+        <div className='container d-flex mt-5'>
+            <div className='line-chart'>
+                <LineChart className='container line-chart' width={500} height={250} data={data}
+                    margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey={"month"} />
+                    <YAxis dataKey={"investment"} />
+                    <Tooltip dataKey={"revenue"} />
+                    <Line type="monotone" dataKey={"sell"} stroke="#8884d8" />
+                    <Line type="monotone" dataKey={"revenue"} stroke="#82ca9d" />
+                </LineChart>
+            </div>
+
+            <div>
+                <BarChart width={500} height={250} data={data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey={"month"} />
+                    <YAxis dataKey={"investment"}/>
+                    <Tooltip />
+                    <Bar dataKey={"revenue"} fill="#8884d8" />
+                    <Bar dataKey="sell" fill="#82ca9d" />
+                </BarChart>
+            </div>
+        </div>
 
 
 
