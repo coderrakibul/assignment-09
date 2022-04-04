1289f393c3da12adea14bb09b6e29f56c4ff4d24
coderrakibul <coder.rakibul@gmail.com>
2022-04-04 18:05:13 +0600

    question part is ready

--- src/component/Blogs/Blogs.js
+++ src/component/Blogs/Blogs.js
@@ -3,7 +3,19 @@ import React from 'react';
 const Blogs = () => {
     return (
         <div className='container mt-5'>
-            <h2 className='text-center'>This is blog page</h2>
+            <h3>Question-01 : What is semantic tag?</h3>
+            <p>Answer:
+                HTML-5 এ সিমান্টিক ট্যাগ গুলো এসেছে। যার দ্বারা ব্রাউজারকে এটা বোঝানো হয় যে কন্টেন্টের ধরন টা কি। যেমন header, nav, section, article, aside footer.
+                এগুলো দিয়ে সহজেই বোঝা যায় কোথায় কোন ধরনের ইলিমেন্ট রাখা আছে। আর ব্রাউজার এবং সার্চ ইঞ্জিনও খুব দ্রুত পড়তে পারে কোনটা কোন টাইপের ইলিমেন্ট।</p>
+
+            <h3>Question-2 : Difference among inline, block and inline_block element.</h3>
+            <p>Answer:
+                (a) inline: inline element হলো যেসব element তাদের নিজ জায়গা যত টুকূ কেবল ততো টুকুই দখল করে রাখে। যেমন; a, span, input etc tag.
+
+                (b) block: block element হলো যেসব element তার নিজ জায়গা ছাড়াও অবশিষ্ট পুরো জায়গা দখল করে রাখে এবং সেখানে কোনো element কে ঢুকতে দেয় না। যেমন; p, h1-h6, div etc tag.
+
+                (c) inline-block: inline block element হলো inline এবং block element এর কম্বিনেশন।
+                অর্থাৎ inline element এর মতো শুধু নিজের জায়গা টুকু নিয়ে থাকবে কিন্তু আচরণ করবে block element এর মতো। block element এর প্রোপার্টি গুলো এখানে ইউজ করা যায়।</p>
         </div>
     );
 };
